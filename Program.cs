3f5ebb9af4e1bb5217c0483248d801c36ce3c82f
Michael Rychagov <msrychagov@edu.hse.ru>
2023-09-12 20:11:50 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hi, Michael!");
+string a = Console.ReadLine();
+Console.WriteLine(a);
